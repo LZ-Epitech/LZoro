690fcec30feeaea0b83d0f7f902e01568bb892c1
Your Name <goulwenn.l@gmail.com>
2024-04-27 00:30:32 +0200

    feat (App.js header.js) > Begin of header adn added to app.js

--- my-app/src/components/all/header.js
+++ my-app/src/components/all/header.js
@@ -1,5 +1,34 @@
-const Header = () ==> {
+function Header() {
     return (
+        <header id="header">
+            <p class="name" id="name">Goulwenn Lazour</p>
+            <ul class="menu" id="menu">
+                <a href="#acceuil">
+                    <li class="accueil" id="accueil" onclick="menuClose()">
+                        Accueil
+                    </li>
+                </a>
+                <a href="#about">
+                    <li class="About" id="About" onclick="menuClose()">À propos</li>
+                </a>
+                <a href="#works">
+                    <li class="work" id="work" onclick="menuClose()">
+                        Projets
+                    </li>
+                </a>
+                <a href="#contact">
+                    <li class="Contact" id="Contact" onclick="menuClose()">
+                        Contact
+                    </li>
+                </a>
+            </ul>
+            <div class="phone-menu phone" id="phonemenu">
+            <div class="phone-trait trait-1">&nbsp;</div>
+            <div class="phone-trait trait-2">&nbsp;</div>
+            <div class="phone-trait trait-3">&nbsp;</div>
+            </div>
+        </header>
+    );
+}
 
-    )
-}   
+export default Header;
