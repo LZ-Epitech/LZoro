34d8f2eff1706eb4c4e3cb12d50d51a2b01abdd9
Your Name <goulwenn.l@gmail.com>
2024-05-13 12:37:30 +0200

    Begin of toprankings

--- my-app/src/components/ranked/TopRankingsBoard.js
+++ my-app/src/components/ranked/TopRankingsBoard.js
@@ -1,10 +1,40 @@
+import { ProfilMinComponent } from '../acceuil/profils/profils';
 import './css/TopRankingsBoard.css';
+
+function TopThree({users})
+{
+    function three(dataUsers) {
+        let lgt = Math.min(3, dataUsers.length);
+        let first3 = [];
+
+        for (let i = 0; i < lgt; i++) {
+            let userWithPlace = { ...dataUsers[i]};
+            first3.push(userWithPlace);
+        }
+        return first3;
+    }
+
+    let listcard = three(users).map(user => {
+        return <ProfilMinComponent key={user.id} persons={user.fields}/>
+    });
+
+    return(
+        <div className="toprankings_board__topthree">
+            <h2 className="toprankings_board__topthree__title">
+                TOP 3
+            </h2>
+            <div className="toprankings_board__topthree__list">
+                {listcard}
+            </div>
+        </div>
+    );
+}
 
 function TopRankingsBoard({users})
 {
     return (
         <div className="toprankingsboard">
-            Name
+            <TopThree users={users} />
         </div>
     );
 }
--- my-app/src/pages/ranked.js
+++ my-app/src/pages/ranked.js
@@ -1,13 +1,31 @@
 // import { StyleSheet, Text, View } from 'react-native'
 
+import { useEffect, useState } from "react";
+import TopRankingsBoard from "../components/ranked/TopRankingsBoard";
+import { getUsersByElo } from "../providers/getUsers";
+
 function Ranked() {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    
-  return (
-   <div>
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const usersData = await getUsersByElo();
+                setUsers(usersData);
+                setIsLoading(false);
+                console.log(usersData);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
 
-Ranked
-   </div>
-  )
+    return (
+        <div>
+            {isLoading ? ( <p>Loading...</p> ) : ( <TopRankingsBoard users={users} /> )}
+        </div>
+    )
 }
 export default Ranked;
