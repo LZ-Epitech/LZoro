5373cfc9fe32463277a6ac71158729bbe23f820d
Your Name <goulwenn.l@gmail.com>
2024-05-13 14:21:17 +0200

    Login system, getProfil, verifyToken and token creation done

--- back/index.js
+++ back/index.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-import getTable from './airtable.js'
+import express from 'express';
+import cors from 'cors';
+import getTable from './airtable.js';
+import jwt from 'jsonwebtoken';
 
 const app = express()
 const port = 3001
@@ -40,10 +41,20 @@ app.get('/matchs', async (req, res) =>{
     res.json(matchs);
 })
 app.get('/matchsFromPlayer', async (req, res) => {
+    const ID = req.body;
+    const matchs = await getMatchsFromPlayer(ID);
+    res.json(matchs);
+})
+app.get('/api/users/login', async (req, res) => {
+    const { email , password } = req.body;
     const matchs = await getMatchsFromPlayer(req.body);
     res.json(matchs);
 })
-
+app.get('/profils', verifyToken, async (req, res) => {
+    const userEmail = req.user.email;
+    const profil = await getProfil(userEmail);
+    res.json(profil);
+})
 app.listen(port, () => {
     console.log(`Node.JS server launched on port [${port}] : http://localhost:${port}`)
 })
@@ -93,7 +104,7 @@ async function getTournaments1v1()
 {
     let tournoi = await getTournaments();
     let tournaments1v1 = tournoi.filter(tour => {
-        return tour.fields.format == "1v1";
+        return tour.fields.format === "1v1";
     })
 
     return tournaments1v1;
@@ -103,7 +114,7 @@ async function getTournaments2v2()
 {
     let tournoi = await getTournaments();
     let tournaments2v2 = tournoi.filter(tour => {
-        return tour.fields.format == "2v2";
+        return tour.fields.format === "2v2";
     })
 
     return tournaments2v2;
@@ -135,9 +146,46 @@ async function getMatchsFromPlayer(player)
     let matchs = getMatchs();
     let matchsPlayer = matchs.filter(match => {
     for (let i = 0; matchs[i] != null; i++) {
-        if (isInMatchs(player, matchs[i]) == 1)
+        if (isInMatchs(player, matchs[i]) === 1)
             matchsPlayer += matchs[i];
     }
         return match.fields.equipe1
     })
+}
+
+async function logIn(username, password)
+{
+    const users = await getUsers();
+    const user = users.find(user => user.fields.email === username);
+    const secretKey = process.env.JWT_SECRET_KEY;
+
+    if (!user) {
+        return res.status(404).json({ message: 'Les données entrées sont incorects' });
+    }
+    if (password !== user.fields.password) {
+        return res.status(404).json({ message: 'Les données entrées sont incorects' });
+    }
+    const token = jwt.sign({ id: user.id, email: user.fields.email }, secretKey, { expiresIn: '1h' });
+
+    return req.json({ token });
+}
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+    try {
+        const decoded = jwt.verify(token, 'votre_secret_key');
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
+async function getProfil(email)
+{
+    const users = await getUsers();
+    const profil = users.find(user => user.fields.email === email);
+    return profil;
 }
