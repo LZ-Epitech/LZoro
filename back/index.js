29eb916b7499db23ee7fa12186875936632745df
Your Name <goulwenn.l@gmail.com>
2024-05-13 11:34:44 +0200

    Created route for best player 1v1 and 2v2

--- back/index.js
+++ back/index.js
@@ -15,6 +15,14 @@ app.get('/users/by/elo', async (req, res) => {
     const users = await getUsersFilteredByElo();
     res.json(users);
 })
+app.get('/users/by/elo/1v1', async (req, res) => {
+    const users = await getUsersFilteredByElo1v1();
+    res.json(users);
+})
+app.get('/users/by/elo/2v2', async (req, res) => {
+    const users = await getUsersFilteredByElo2v2();
+    res.json(users);
+})
 app.get('/tournaments', async (req, res) => {
     const tournaments = await getTournaments();
     res.json(tournaments);
@@ -55,6 +63,20 @@ async function getUsersFilteredByElo()
 {
     const users = await getUsers();
     const sortedUsers = users.sort((a, b) => (b.fields.elo1v1 + b.fields.elo2v2) - (a.fields.elo1v1 + a.fields.elo2v2));
+
+    return sortedUsers;
+}
+async function getUsersFilteredByElo1v1()
+{
+    const users = await getUsers();
+    const sortedUsers = users.sort((a, b) => b.fields.elo1v1 - a.fields.elo1v1);
+
+    return sortedUsers;
+}
+async function getUsersFilteredByElo2v2()
+{
+    const users = await getUsers();
+    const sortedUsers = users.sort((a, b) => b.fields.elo2v2 - a.fields.elo2v2);
 
     return sortedUsers;
 }
