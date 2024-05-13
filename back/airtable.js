ca0b455086aa5293fa9cb89eecd49f7fd1fa991a
Your Name <goulwenn.l@gmail.com>
2024-05-13 15:49:13 +0200

    postInTable done

--- back/airtable.js
+++ back/airtable.js
@@ -1,28 +1,5 @@
 import Airtable from 'airtable';
 
-// function getTable(table)
-// {
-//     let reco = [];
-
-//     Airtable.configure({
-//         endpointUrl: 'https://api.airtable.com',
-//         apiKey: 'pati03tXLIyNErENB.23c5d11e76903f964781771029971b5b646f660b3d090edcf2fc5ccd962b2d66'
-//     });
-//     var base = Airtable.base('appBAcxHY6p1Dpv3p');
-
-//     base(table).select({
-//         view: "Grid view"
-//     }).eachPage(function page(records, fetchNextPage) {
-//         records.forEach(function(record) {
-//             reco.push(record);
-//         });
-//         fetchNextPage();
-//     }, function done(err) {
-//         if (err) { console.error(err); return; }
-//     });
-//     return reco;
-// }
-
 function getTable(table) {
     Airtable.configure({
         endpointUrl: 'https://api.airtable.com',
@@ -51,5 +28,33 @@ function getTable(table) {
     });
 }
 
+function postInTable(table, data)
+{
+    var base = new Airtable({apiKey: '23c5d11e76903f964781771029971b5b646f660b3d090edcf2fc5ccd962b2d66'}).base('appBAcxHY6p1Dpv3p');
 
-export default getTable;
+    const obj = {};
+    data.forEach(item => {
+        const key = item[0];
+        const value = item[1];
+        obj[key] = value;
+    });
+    base(table).create([
+        {
+            "fields": obj,
+        }
+    ], function(err, records) {
+        if (err) {
+            console.error(err);
+            return null;
+        }
+        if (records && records.length > 0) {
+            const firstRecordId = records[0].getId();
+            return firstRecordId;
+        } else {
+            console.log('Aucun enregistrement créé.');
+            return;
+        }
+    });
+}
+
+export { getTable, postInTable };
