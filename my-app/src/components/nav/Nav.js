cf133fb227ce4e14832c8fbf85b37f34f993ae8d
yoyo <yoann.pidoux@epitech.eu>
2024-05-14 08:34:50 +0200

    add --> backend login discord

--- my-app/src/components/nav/Nav.js
+++ my-app/src/components/nav/Nav.js
@@ -4,17 +4,42 @@ import './css/nav.css';
 
 function Nav() {
     const [discordUser, setDiscordUser] = useState(null);
-    const [discordUserName, setDiscordUserName] = useState(null);
-    const handleLogin = () => {
-        window.location.href = `https://discord.com/oauth2/authorize?client_id=1239797278533746732&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=identify`;
+
+    useEffect(() => {
+        const extractTokenFromUrl = () => {
+            const urlParams = new URLSearchParams(window.location.hash.substr(1));
+            const accessToken = urlParams.get('access_token');
+            if (accessToken) {
+                localStorage.setItem('discordToken', accessToken);
+                window.history.replaceState({}, document.title, "/");
+                fetchDiscordUser(accessToken);
+            }
+        };
+
+        extractTokenFromUrl();
+    }, []);
+
+    const fetchDiscordUser = async (accessToken) => {
+        const response = await fetch('https://discord.com/api/users/@me', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        if (response.ok) {
+            const user = await response.json();
+            setDiscordUser(user);
+        } else {
+            console.error('Failed to fetch Discord user');
+        }
     };
 
     const handleLogout = () => {
-        // Supprimer le token Discord du localStorage
         localStorage.removeItem('discordToken');
-        // Réinitialiser l'utilisateur Discord
         setDiscordUser(null);
-        setDiscordUserName(null); // Réinitialiser le nom d'utilisateur Discord
+    };
+
+    const handleLogin = () => {
+        window.location.href = `https://discord.com/oauth2/authorize?client_id=1239797278533746732&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=identify`;
     };
 
     return (
@@ -29,11 +54,10 @@ function Nav() {
                     <div>
                         <img src={`https://cdn.discordapp.com/avatars/${discordUser.id}/${discordUser.avatar}.png`} alt="Discord Avatar" />
                         <button className="logout-button" onClick={handleLogout}>Logout</button>
+                        <p>{discordUser.username}</p>
                     </div>
                 ) : (
-                    <button className="login-button" onClick={handleLogin}>
-                        {discordUserName ? `Logged in as ${discordUserName}` : 'Login with Discord'}
-                    </button>
+                    <button className="login-button" onClick={handleLogin}>Login with Discord</button>
                 )}
             </div>
         </div>
